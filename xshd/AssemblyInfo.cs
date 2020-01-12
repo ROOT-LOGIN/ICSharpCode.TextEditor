f2ac3b5159dd07f57aa1b34ce15797dcc59af962
ROOT-LOGIN <22186123+ROOT-LOGIN@users.noreply.github.com>
2020-01-12 18:31:38 +0800

    Add files via upload
    part4

---
+++ xshd/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+/*
+[assembly: AssemblyCompany("ic#code")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("2000-2010 AlphaSierraPapa")]
+[assembly: AssemblyDescription("A .NET text editor control")]
+[assembly: AssemblyProduct("SharpDevelop")]
+[assembly: AssemblyTitle("ICSharpCode.TextEditor")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("3.2.1.6466")]
+[assembly: CLSCompliant(true)]
+[assembly: CompilationRelaxations(8)]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
+*/
+
+[assembly: StringFreezing]
+
